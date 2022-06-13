13250698edb8cfc6fd5bc4d596ac96fcbc1b7256
Umang Kaushik <umangkaushk2410@gmail.com>
2022-06-13 23:28:03 +0530

    final commit

---
+++ classifier.py
@@ -0,0 +1,44 @@
+import streamlit as st 
+from fastai.vision.all import *
+from PIL import Image
+import pathlib
+import urllib.request
+
+temp = pathlib.PosixPath
+plt = platform.system()
+if plt == 'Windows': pathlib.PosixPath = pathlib.WindowsPath
+
+path = Path()
+path.ls(file_exts='.pkl')
+
+learn_inf = load_learner(path/'export.pkl', cpu=True)
+# out_pl = st.image(load_image(image), width=250)
+
+
+def load_image(img_file):
+    img = PILImage.create(img_file)
+    return img
+
+def on_click_classify(image):
+    # load_image(image)
+    out_pl = st.image(load_image(image), width=250)
+    pred, pred_idx, probs = learn_inf.predict(load_image(image))
+    st.write('Prediction: ', str(pred)[10:], '; Probability: ', float(probs[pred_idx]))
+    
+
+st.title('Dog Classifier')
+st.header('Choose/Click your Dog!!')
+image = st.file_uploader(label='Choose your dog!', type=['png', 'jpg'], key='img', help='upload an img of dog')
+
+picture = st.camera_input(label='Click your dog!')
+
+
+# btn_run.on_change(on_click_classify)
+btn_run = st.button(label='Classify')
+if btn_run:
+    if image: on_click_classify(image)
+    else: on_click_classify(picture)
+    
+st.markdown('#### Created by **Umang Kaushik**')
+st.markdown('##### **[Github](https://github.com/Umang-10)**')
+
